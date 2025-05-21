0f7801b8bd339d2099afb8440ace89167907b7bd
HAO <147186777+KARENLHAO@users.noreply.github.com>
2025-05-21 16:50:26 +0000

    first commit

---
+++ src/components/echart/centerLeft/centerLeft1Chart/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Chart :cdata="cdata" />
+  </div>
+</template>
+
+<script>
+import Chart from './chart.vue';
+export default {
+  data () {
+    return {
+      cdata: {
+        xData: ["冲压", "焊接", "涂装", "质检", "总装", "包装出货"],
+        seriesData: [
+          { value: 10, name: "冲压" },
+          { value: 5, name: "焊接" },
+          { value: 15, name: "涂装" },
+          { value: 25, name: "质检" },
+          { value: 20, name: "总装" },
+          { value: 35, name: "包装出货" }
+        ]
+      }
+    }
+  },
+  components: {
+    Chart,
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
